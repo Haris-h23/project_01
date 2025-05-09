d3b9448795ce4a34dd4aa7a5dd620f3a3485aff3
Haris-h23 <s235775@student.stu.dk>
2025-05-09 15:22:57 +0200

    tilføjet 'projectManagementSystem.java' fil og dens funktioner

---
+++ src/main/java/dtu/example/ui/softwarehouse/ProjectManagementSystem.java
@@ -0,0 +1,26 @@
+package dtu.example.ui.softwarehouse;
+
+import java.util.*;
+
+public class ProjectManagementSystem {
+    private List<Project> projects = new ArrayList<>();
+    private Map<String, Employee> employees = new HashMap<>();
+
+    public Project createProject(String name) {
+        Project project = new Project(name);
+        projects.add(project);
+        return project;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public Employee getOrCreateEmployee(String initials) {
+        return employees.computeIfAbsent(initials.toLowerCase(), Employee::new);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return new ArrayList<>(employees.values());
+    }
+}
