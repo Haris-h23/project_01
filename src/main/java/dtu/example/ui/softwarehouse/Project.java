1fe06dcb882fae995686d001c0a36183552e5c69
Haris-h23 <s235775@student.stu.dk>
2025-05-09 13:37:28 +0200

    tilføjet 'Project.java' og dens implementering

---
+++ src/main/java/dtu/example/ui/softwarehouse/Project.java
@@ -0,0 +1,43 @@
+package dtu.example.ui.softwarehouse;
+
+import java.util.*;
+
+public class Project {
+    private static int projectCounter = 25000;
+    private String projectNumber;
+    private String projectName;
+    private String projectLeaderInitials;
+    private List<Activity> activities = new ArrayList<>();
+    private boolean isDone = false;
+
+    public Project(String name) {
+        this.projectNumber = String.valueOf(++projectCounter);
+        this.projectName = name;
+    }
+
+    public String getProjectNumber() { return projectNumber; }
+    public String getProjectName() { return projectName; }
+
+    public void setProjectLeader(String initials) {
+        this.projectLeaderInitials = initials.toLowerCase();
+    }
+
+    public String getProjectLeader() {
+        return projectLeaderInitials;
+    }
+
+    public boolean isLeader(String initials) {
+        return projectLeaderInitials != null && projectLeaderInitials.equalsIgnoreCase(initials);
+    }
+
+    public void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    public boolean isDone() { return isDone; }
+    public void markAsDone() { this.isDone = true; }
+}
